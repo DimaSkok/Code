8bb0ce58736c1c25d71999e87502f070e832db69
DimaSkok <skok.dmitrii@phystech.edu>
2024-03-31 12:27:17 +0300

    analase the signal

--- 1 — копия (2).py
+++ 1 — копия (2).py
@@ -1,61 +1,59 @@
-from skyfield.api import EarthSatellite, load
-import math
+from scipy.io import wavfile
+from scipy.signal import hilbert, resample
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
 
-R = 6356.7523
-pi = 3.141592653589793
+rate = 2080 * 2
+samplerate, data = wavfile.read('signal (1).wav')
+data = np.array(data, dtype=float)
+data = data - 128
+data = data / 128
+data_h = hilbert(data)
+A_data_h = np.abs(data_h)
 
-data = load('de421.bsp')
-line1 = "1 33591U 09005A   22256.82151620  .00000034  00000-0  20523-4 0 9999"
-line2 = "2 33591 099.0678 182.1456 0014237 307.7083   .2897 14.12431814670202"
-satellite = EarthSatellite(line1, line2, name='SAT')
+resampled = resample(A_data_h, int(A_data_h.shape[0] * rate / samplerate))
+normalized = ((resampled - min(resampled)) / ((max(resampled) - min(resampled))))
 
-ts = load.timescale()
+A_data_h = normalized
 
-# Координаты антенны (в данном случае примерные значения)
-antenna_lat = math.radians(37.7749) # Широта Сан-Франциско
-antenna_lon = math.radians(-122.4194) # Долгота Сан-Франциско
-antenna_alt_km = 0.1 # Высота антенны над уровнем моря в км
+synh = np.array((0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0)) * 255
+skip = 909
+high = len(A_data_h) // 2080
+x = len(A_data_h) - high * 2080
 
-# Создаем список всех возможных комбинаций времени
-times_list = []
-for day in range(29,30):
-    for hour in range(17,24):
-        for minute in range(60):
-            times_list.append(ts.utc(2024, 2, day, hour, minute))
+NA_data_h = A_data_h[:-x]
 
-for day in range(1):
-    for hour in range(18):
-        for minute in range(48):
-            times_list.append(ts.utc(2024, 3, day+1, hour, minute))
+data_temp = NA_data_h * 255
+data = data_temp.astype(int).reshape(-1, 2080)
 
-# Итерируем по всем комбинациям времени
-for t in times_list:
-    geocentric = satellite.at(t)
-    subpoint = geocentric.subpoint()
-
-    shir_sat = subpoint.latitude.radians
-    dol_sat = subpoint.longitude.radians
-    h_sat = subpoint.elevation.km
-
-    x_sat = (h_sat + R) * math.cos(shir_sat) * math.cos(dol_sat)
-    y_sat = (h_sat + R) * math.cos(shir_sat) * math.sin(dol_sat)
-    z_sat= (h_sat + R) * math.sin(shir_sat)
-
-    x_antenna= (antenna_alt_km + R) * math.cos(antenna_lat) * math.cos(antenna_lon)
-    y_antenna= (antenna_alt_km + R) * math.cos(antenna_lat) * math.sin(antenna_lon)
-    z_antenna= (antenna_alt_km + R) * math.sin(antenna_lat)
+checker = True
+for i in range(high):
+    for j in range(5512):
+        flag = False
+        check = data[i][j:j + 40] == synh
+        for ch in synh:
+            checker = checker and ch
+        if (checker == True) and (flag == False):
+            j += skip
+        if (checker == True):
+            data[i] = data[i][j:-1].append(data[i][0:j])
 
 
-    dx = x_sat - x_antenna
-    dy = y_sat - y_antenna
-    dz = z_sat - z_antenna
 
-    az = math.atan2(dy, dx)
-    el = math.atan2(dz, math.sqrt(dx*dx + dy*dy))
 
-    az_deg = math.degrees(az)
-    el_deg = math.degrees(el)
 
-    print(f"At time {t.utc_strftime('%Y-%m-%d %H:%M:%S')}:")
-    print(f"Azimuth: {az_deg} degrees")
-    print(f"Elevation: {el_deg} degrees")
+plt.imshow(data, cmap='gray', aspect='auto')
+plt.axis('off')
+plt.show()
+
+
+
+
+
+#figsize = (16, 5)
+#plt.figure(figsize=figsize)
+#plt.plot(data[229*samplerate: 230*samplerate])
+#plt.plot(data_h[229*samplerate: 230*samplerate])
+#plt.plot(A_data_h)
+#plt.show()
--- прямая от гпт.py
+++ прямая от гпт.py
@@ -4,47 +4,34 @@ from matplotlib.ticker import MultipleLocator
 
 # Данные точек (x, y)
 
-y = np.array([
-580,
-560,
-430,
-360,
-330,
-260,
-220,
-190,
-160,
-140,
-130,
-110,
-100,
-93,
-86,
-80,
-76,
-72,
-
-
+x = np.array([
+0.20,
+0.40,
+0.61,
+0.81,
+0.92
 ])
-const = 600
-x = np.ones([len(y)])
-y = np.log(const/y)
-
+y = np.array([
+1.4,
+2.8,
+4.4,
+5.8,
+6.7
+])
 
 sum = 0
-for i in range(1, len(x)):
-    x[i] += x[i - 1]
 
 # Аппроксимация прямой
 A = np.vstack([x, np.ones(len(x))]).T
 #k, b = np.linalg.lstsq(A, y, rcond=None)[0]
 k, b = np.sum(x * y) / np.sum(x**2), 0
 
+
 fig, ax = plt.subplots()
 
 for i in range(len(x)):
-    sum += (y[i] - k * x[i] + b) ** 2
-pogr = str((sum / len(x)) ** 1/2)[:6]
+    sum += ((y[i] - k * x[i] - b) / (k * x[i] + b)) ** 2
+pogr = str((sum / len(x)) ** (1/2))[:6]
 
 
 print("Коэффициенты:")
@@ -55,20 +42,32 @@ print('Погрешнсть')
 print(pogr)
 
 
+
+
 # Построение графика
 plt.plot(x, y, '+', color='red') # Точки
 plt.plot(x, k*x + b) # Прямая
-plt.text(1, 2, 'Погр. = ' + pogr, color='blue')
+
+'''
+plt.plot([175, 175], [26.6, 35], color='b', linestyle=':', linewidth=1)
+plt.plot([226, 226], [26, 34], color='b', linestyle=':', linewidth=1)
+plt.plot([200.5, 200.5], [26.6, 34.2], color='b', linestyle=':', linewidth=2)
+'''
+
+plt.text(0.3, 6, 'Погр. = ' + pogr, color='blue')
+#plt.text(25, 22.45, 'R0 = ' + str(b)[:6], color='blue')
+#plt.text(25, 22.3, 'k = ' + str(k)[:6], color='blue')
+
 ax.set_axisbelow(True)
 plt.grid(True, color='lightgray', linestyle='--')
-plt.xlabel("t,c")
-plt.ylabel("ln(P0/P)")
+plt.xlabel("N, Вт")
+plt.ylabel("ΔT,⁰С")
 title_text = 'Рисунок 2'
 ax.set_title(title_text, wrap=True)
-#plt.axis([0, 0.6, 0, 300])
-ax.yaxis.set_major_locator(MultipleLocator(0.1))
-ax.yaxis.set_minor_locator(MultipleLocator(0.05))
-ax.xaxis.set_major_locator(MultipleLocator(1))
-ax.xaxis.set_minor_locator(MultipleLocator(1))
+#plt.axis([0, 500, 26, 35])
+ax.yaxis.set_major_locator(MultipleLocator(0.5))
+ax.yaxis.set_minor_locator(MultipleLocator(0.1))
+ax.xaxis.set_major_locator(MultipleLocator(0.1))
+ax.xaxis.set_minor_locator(MultipleLocator(0.02))
 plt.show()
 
